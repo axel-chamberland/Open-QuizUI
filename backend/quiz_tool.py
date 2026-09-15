0f0938fa52828d0fe9cf51845e0fab76e9e401d1
Axel <axel19.chamberland@gmail.com>
2026-09-14 23:29:09 -0400

    release: bumb version to 2.2.0

--- backend/quiz_tool.py
+++ backend/quiz_tool.py
@@ -3,7 +3,7 @@ title: QuizUI
 author: Axel Chamberland
 git_url: https://github.com/axel-chamberland/OpenQuizUI
 description: This tool allows large language models to generated interactive multiple-choice quizzes.
-version: 2.1.1
+version: 2.2.0
 licence: MIT
 """
 
