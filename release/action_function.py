28884b84ba2a4fcb6fc4433bff8097fbf2bf80b0
djxel <axel19.chamberland@gmail.com>
2026-09-15 13:22:16 -0400

    release: bump version to 2.2.1 after bug fix

--- release/action_function.py
+++ release/action_function.py
@@ -3,7 +3,7 @@ title: QuizUI
 author: Axel Chamberland
 git_url: https://github.com/axel-chamberland/OpenQuizUI
 description: Converts a multiple-choice quiz message into an interactive HTML quiz.
-version: 2.2.0
+version: 2.2.1
 """
 
 import random
